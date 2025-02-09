cc544a8e4e124612c77669d96720871069bfee3a
RV <rvbenu@gmail.com>
2025-02-08 22:58:54 -0500

    Enabled sign column

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+
+-- Always show the sign column to prevent layout shifts
+vim.opt.signcolumn = "yes"
+
 -- width of a tab
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
