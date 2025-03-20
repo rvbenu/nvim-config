05748c0d900c2fbcd0c80dfbcf2b90293c846f2a
RV <rvbenu@gmail.com>
2025-03-20 01:16:04 -0400

    pyright and completion suggestions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,27 +65,5 @@ return {
             })
         end,
     },
-
-    {   -- Formatting & Linting with null-ls
-        "jose-elias-alvarez/null-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.prettier,
-                    null_ls.builtins.diagnostics.eslint,
-                },
-            })
-
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*",
-                callback = function()
-                    vim.lsp.buf.format()
-                end,
-            })
-        end,
-    },
 }
 
